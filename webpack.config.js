c85ca816a04653ce96c273f5374ee0ecf7182710
Justin <justinrkinsman@gmail.com>
2022-08-11 11:55:35 -0300

    Attempt to fix webpack issue

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,18 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   resolve: {
+    modules: [...],
     fallback: {
-      util: require.resolve("util/")
-    }
+      "fs": false,
+      "tls": false,
+      "net": false,
+      "path": false,
+      "zlib": false,
+      "http": false,
+      "https": false,
+      "stream": false,
+      "crypto": false,
+      "crypto-browserify": require.resolve('crypto-browserify'), //if you want to use this module also don't forget npm i crypto-browserify 
+    } 
   }
 };
